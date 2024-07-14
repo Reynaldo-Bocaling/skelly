d90edbd162caec017b2e41a00bb14be59b11a610
Reynaldo <reynaldobocaling@gmail.com>
2024-07-14 13:53:21 +0800

    update project

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,7 +4,7 @@ import { social } from "../constants";
 const Social = () => {
   return (
     <div className="relative z-[999] w-full">
-      <div className=" py-4 px-5 flexAround w-full  md:max-w-3xl mx-auto mt-40 md:mt-7">
+      <div className=" py-4 px-12 md:px-5 flexAround w-full  md:max-w-3xl mx-auto mt-52 md:mt-7">
         {social.map((item, index) => (
           <a
             key={index}
@@ -14,7 +14,7 @@ const Social = () => {
             <img
               src={item.icon}
               alt=""
-              className="w-[100px] h-[100px] md:w-[40px] md:h-[40px]"
+              className="w-[80px] h-[80px] md:w-[40px] md:h-[40px]"
             />
           </a>
         ))}
